96058680a9633462d29621eb2fabd0f5303fc217
macroblock <undefined.subject@gmail.com>
2018-10-26 21:46:40 +1000

    sync

--- pkg/zflag/main.go
+++ pkg/zflag/main.go
@@ -6,6 +6,8 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/macroblock/imed/pkg/misc"
+
 	"github.com/macroblock/imed/pkg/zlog/zlog"
 )
 
@@ -16,18 +18,20 @@ var (
 // TFlag -
 type TFlag struct {
 	arg      func()
-	cmd      func() //, entry tEntry)
+	cmd      func() error
 	keys     []string
 	variable interface{}
 	brief    string
 	usage    string
 	hint     string
+	fullDoc  string
 }
 
 // TSection -
 type TSection struct {
 	TFlag
 
+	name string
 	// flags    []*TFlag
 	// sections []*TSection
 	elements []Interface
@@ -63,13 +67,28 @@ func (o *TFlag) Do() {
 func (o *TFlag) Keys() []string { return o.keys }
 
 // Brief -
-func (o *TFlag) Brief() string { return o.brief }
+func (o *TFlag) Brief() string {
+	if o.brief == "" {
+		return ""
+	}
+	return fmt.Sprintf("%v\n", o.brief)
+}
 
 // Usage -
-func (o *TFlag) Usage() string { return o.usage }
+func (o *TFlag) Usage() string {
+	if o.usage == "" {
+		return ""
+	}
+	return fmt.Sprintf("\nUsage:\n    %v\n", o.usage)
+}
 
 // Hint -
-func (o *TFlag) Hint() string { return o.hint }
+func (o *TFlag) Hint() string {
+	if o.hint == "" {
+		return ""
+	}
+	return fmt.Sprintf("\n%v\n", o.hint)
+}
 
 func splitKeys(keys string) []string {
 	ret := []string{}
@@ -83,28 +102,17 @@ func splitKeys(keys string) []string {
 }
 
 // NewSection -
-func NewSection(name string, cmd func(), brief, usage, hint string, elements ...Interface) *TSection {
+func NewSection(name string, fn func() error, brief, usage, hint, fullDoc string, elements ...Interface) *TSection {
 	section := &TSection{}
-	// section.keys = splitKeys(keys)
-	section.keys = []string{name}
-	section.cmd = cmd
+	section.name = name
+	if name != "" {
+		section.keys = []string{name}
+	}
+	section.cmd = fn
 	section.brief = brief
 	section.usage = usage
 	section.hint = hint
-	// for _, item := range elements {
-	// 	switch t := item.(type) {
-	// 	default:
-	// 		log.Panic(true, "unreachable")
-	// 	case *TFlag:
-	// 		if t != nil {
-	// 			section.flags = append(section.flags, t)
-	// 		}
-	// 	case *TSection:
-	// 		if t != nil {
-	// 			section.sections = append(section.sections, t)
-	// 		}
-	// 	}
-	// }
+	section.fullDoc = fullDoc
 	section.elements = elements
 	log.Warning(len(section.keys) == 0, "section without key(s)")
 	log.Warning(len(section.elements) == 0, "section without flag(s)")
@@ -113,7 +121,7 @@ func NewSection(name string, cmd func(), brief, usage, hint string, elements ...
 }
 
 // New -
-func New(keys string, variable interface{}, brief, usage, hint string) *TFlag {
+func New(keys string, variable interface{}, fn func() error, brief, usage, hint, fullDoc string) *TFlag {
 	flag := &TFlag{}
 	flag.keys = splitKeys(keys)
 	flag.variable = variable
@@ -121,33 +129,18 @@ func New(keys string, variable interface{}, brief, usage, hint string) *TFlag {
 	flag.brief = brief
 	flag.usage = usage
 	flag.hint = hint
+	flag.fullDoc = fullDoc
 	log.Warning(len(flag.keys) == 0, "flag without key(s)")
 	log.Warning(flag.cmd == nil, "flag without func")
 	return flag
 }
 
-// ElemByKey -
-func ElemByKey(section *TSection, key string) Interface {
-	var ret Interface
-	for _, elem := range section.elements {
-		for _, k := range elem.Keys() {
-			if k == key {
-				return elem
-			}
-			if k == "" && ret == nil {
-				ret = elem
-			}
-		}
-	}
-	return ret
-}
-
-// NextElem -
-func NextElem(section *TSection, args *[]string) (Interface, error) {
-	if len(*args) == 0 {
+// FindKeyHandler -
+func FindKeyHandler(section *TSection, args []string) (Interface, error) {
+	if len(args) == 0 {
 		return nil, nil
 	}
-	key := (*args)[0]
+	key := args[0]
 	ret := Interface(nil)
 	for _, elem := range section.elements {
 		for _, k := range elem.Keys() {
@@ -159,30 +152,37 @@ func NextElem(section *TSection, args *[]string) (Interface, error) {
 			}
 		}
 	}
-	if ret == nil {
-		return nil, fmt.Errorf("section %q has an unsupported key %q", section.keys[0], key)
-	}
 	return ret, nil
 }
 
 // Parse -
 func (o *TSection) Parse(args *[]string) error {
 	if len(*args) == 0 {
-		return fmt.Errorf("%v", "not enough parameters")
+		return fmt.Errorf("%v", "something went wrong")
 	}
+	// get the correct name prefix for an error reporting
+	errPrefix := o.name
+	if errPrefix != "" {
+		errPrefix = fmt.Sprintf("<%v> has an ", errPrefix)
+	}
+
 	*args = (*args)[1:]
-	elem, err := NextElem(o, args)
-	if err != nil {
-		return err
-	}
-	for elem != nil {
-		err := elem.Parse(args)
+	for len(*args) > 0 {
+		elem, err := FindKeyHandler(o, *args)
 		if err != nil {
 			return err
 		}
-		elem, err = NextElem(o, args)
+		if elem == nil {
+			return fmt.Errorf("%vunsupported key %q or a parameter without a key", errPrefix, (*args)[0])
+		}
+		err = elem.Parse(args)
 		if err != nil {
-			return err
+			switch elem.(type) {
+			default:
+				return fmt.Errorf("%v%v", errPrefix, err)
+			case *TSection:
+				return fmt.Errorf("%v", err)
+			}
 		}
 	}
 	return nil
@@ -190,21 +190,79 @@ func (o *TSection) Parse(args *[]string) error {
 
 // Parse -
 func (o *TFlag) Parse(args *[]string) error {
+	if len(*args) == 0 {
+		return fmt.Errorf("%v", "something went wrong")
+	}
 	key := (*args)[0]
 	*args = (*args)[1:]
+	// a boolean key doesn't have an argument so it should be processing first
 	if t, ok := o.variable.(*bool); ok {
 		*t = true
-	}
-	*args = (*args)[1:]
-	switch t := o.variable.(type) {
-	case *bool:
-		*t = true
 		return nil
+	}
+	if len(*args) == 0 {
+		return fmt.Errorf("a key %q of type %T without a parameter", key, o.variable)
+	}
+	arg := (*args)[0]
+	switch t := o.variable.(type) {
+	default:
+		return fmt.Errorf("an unsupported key of %T type", o.variable)
 	case *string:
 		*t = arg
 	case *[]string:
 		*t = append(*t, arg)
 	}
 	*args = (*args)[1:]
+	return nil
+}
+
+func addNewLine(s string) string {
+	if s != "" {
+		return s + "\n"
+	}
+	return s
+}
+
+func formatFlags(elements []Interface, prefix string) string {
+	if len(elements) == 0 {
+		return ""
+	}
+	maxKeyStr := 0
+	lines := []string{}
+	for _, elem := range elements {
+		keys := strings.Join(elem.Keys(), ", ")
+		maxKeyStr = misc.MaxInt(maxKeyStr, len(keys))
+		lines = append(lines, keys)
+	}
+	for i, elem := range elements {
+		lines[i] = fmt.Sprintf("    %v %v  %v", lines[i], strings.Repeat(" ", maxKeyStr-len(lines[i])), elem.Brief())
+	}
+	return fmt.Sprintf("\nThe %s are:\n%v", prefix, strings.Join(lines, ""))
+}
+
+// PrintHelp -
+func (o *TSection) PrintHelp() error {
+
+	flags := []Interface{}
+	sections := []Interface{}
+	for _, elem := range o.elements {
+		switch t := elem.(type) {
+		default:
+			return fmt.Errorf("something went wrong")
+		case *TFlag:
+			flags = append(flags, t)
+		case *TSection:
+			sections = append(sections, t)
+		}
+	}
+	text := fmt.Sprintf("%v%v%v%v%v", o.Brief(), o.Usage(),
+		formatFlags(flags, "flags"),
+		formatFlags(sections, "sections"),
+		o.Hint(),
+	)
+	text = strings.Replace(text, "!progname!", programName, -1)
+
+	fmt.Print(text)
+
 	return nil
 }
