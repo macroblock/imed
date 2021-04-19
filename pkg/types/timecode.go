1a21fdd7e504269dee8226802bce96e09b2cd098
mike <undefined@subject.com>
2021-04-19 16:56:47 +1000

    new package types

---
+++ pkg/types/timecode.go
@@ -0,0 +1,190 @@
+package types
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type (
+	Timecode float64
+
+	HHMMSSMs struct {
+		HH, MM, SS, Ms int
+	}
+)
+
+var (
+	globalMutex = sync.Mutex{}
+	globalFps float64 = 25.0
+
+	errHHMMSSMsTemplate = "%v parse error: %v"
+)
+
+var errval = Timecode(math.NaN())
+
+// String -
+func (o HHMMSSMs) String() string {
+	if o.HH >= 0 && o.MM >= 0 && o.SS >= 0 && o.Ms >= 0 {
+		return fmt.Sprintf("%02v:%02v:%02v.%03v", o.HH, o.MM, o.SS, o.Ms)
+	}
+	return fmt.Sprintf("-%02v:%02v:%02v.%03v", abs(o.HH), abs(o.MM), abs(o.SS), abs(o.Ms))
+}
+
+func errMsg(val string, err error) error {
+	return fmt.Errorf(errHHMMSSMsTemplate, val, err)
+}
+
+func SetFps(fps float64) {
+	if fps <= 0.0 {
+		panic("SetFps(): timecode fps cannot be <= 0")
+	}
+	globalMutex.Lock()
+	defer globalMutex.Unlock()
+	globalFps = fps
+}
+
+func GetFps() float64 {
+	globalMutex.Lock()
+	defer globalMutex.Unlock()
+	ret := globalFps
+	return ret
+}
+
+// NewTimecode -
+func NewTimecode(h, m, s float64) Timecode {
+	return Timecode((h*60+m)*60 + s)
+}
+
+// ParseTimecode -
+func ParseTimecode(t string) (Timecode, error) {
+	ret, err := ParseHHMMSS(t)
+	if err != nil {
+		e := error(nil)
+		ret, e = ParseHHMMSSFr(t)
+		if e != nil {
+			return Timecode(0), err
+		}
+	}
+	return ret, nil
+}
+
+// ParseHHMMSS - hh:mm:ss format where ss can be float64
+func ParseHHMMSS(t string) (Timecode, error) {
+	o := Timecode(0)
+	// fmt.Printf("here %v", t)
+	x := strings.Split(t, ":")
+	i := len(x)
+	if i > 3 {
+		return o, errMsg(t, fmt.Errorf("too many colons"))
+	}
+	h, m, s := 0.0, 0.0, 0.0
+	signbit := false
+	err := error(nil)
+	i--
+	s, signbit, err = parseHelper(x, i, true, signbit, err)
+	i--
+	m, signbit, err = parseHelper(x, i, false, signbit, err)
+	i--
+	h, signbit, err = parseHelper(x, i, false, signbit, err)
+	if err != nil {
+		return errval, err
+	}
+	ret := (h*60+m)*60 + s
+	if signbit {
+		return Timecode(-ret), nil
+	}
+	return Timecode(ret), nil
+}
+
+// ParseHHMMSSFr - hh:mm:ss:fr format
+func ParseHHMMSSFr(t string) (Timecode, error) {
+	fps := GetFps()
+	errval := Timecode(math.NaN())
+	x := strings.Split(t, ":")
+	i := len(x)
+	if i > 4 {
+		return errval, errMsg(t, fmt.Errorf("too many colons"))
+	}
+	h, m, s, fr := 0.0, 0.0, 0.0, 0.0
+	signbit := false
+	err := error(nil)
+	if i == 4 {
+		i--
+		fr, signbit, err = parseHelper(x, i, false, signbit, err)
+	}
+	i--
+	s, signbit, err = parseHelper(x, i, false, signbit, err)
+	i--
+	m, signbit, err = parseHelper(x, i, false, signbit, err)
+	i--
+	h, signbit, err = parseHelper(x, i, false, signbit, err)
+	if err != nil {
+		return errval, err
+	}
+	ret := (h*60+m)*60 + s + fr/fps
+	if signbit {
+		return Timecode(-ret), nil
+	}
+	return Timecode(ret), nil
+}
+
+// HHMMSSMs -
+func (o Timecode) HHMMSSMs() HHMMSSMs {
+	v := int(o*1000)
+	ms := v % 1000
+	v = v / 1000
+	s := v % 60
+	v = v / 60
+	m := v % 60
+	h := v / 60
+	return HHMMSSMs{HH: h, MM: m, SS: s, Ms: ms}
+}
+
+// String -
+func (o Timecode) String() string {
+	return o.HHMMSSMs().String()
+}
+
+// InSeconds -
+func (o Timecode) InSeconds() float64 {
+	return float64(o)
+}
+
+func parseHelper(s []string, index int, isFloat, signbit bool, err error) (float64, bool, error) {
+	if index < 0 || err != nil {
+		return 0.0, signbit, err
+	}
+	NaN := math.NaN()
+	v := 0.0
+	if isFloat {
+		v, err = strconv.ParseFloat(s[index], 64)
+	} else {
+		x, e := strconv.Atoi(s[index])
+		v = float64(x)
+		err = e
+	}
+	if err != nil {
+		return NaN, false, errMsg(strings.Join(s, ":"), err)
+	}
+	sb := math.Signbit(v)
+
+	// if it is the firstmost subvalue
+	if index == 0 {
+		return math.Abs(v), sb, nil
+	}
+	if sb {
+		return NaN, false, errMsg(strings.Join(s, ":"), fmt.Errorf("only first subvalue can have a sign"))
+	}
+	return v, signbit, nil
+}
+
+func abs(v int) int {
+	if v < 0 {
+		return -v
+	}
+	return v
+}
+
---
+++ pkg/types/timecode_test.go
@@ -0,0 +1,116 @@
+package types
+
+import (
+	// "fmt"
+	// "strings"
+	"testing"
+
+	// "unsafe"
+)
+
+func hhmmssms(hh, mm, ss, ms int) Timecode {
+	return Timecode((float64(hh)*60+float64(mm))*60+float64(ss) + float64(ms)/1000)
+}
+
+type correct struct {
+	in string
+	out Timecode
+}
+
+type incorrect struct {
+	in string
+	out string
+}
+
+var typesParseHHMMSSCorrect = []correct {
+	correct{ "01:02:03.456", NewTimecode(1, 2, 3.456) },
+	correct{ "1", NewTimecode(0, 0, 1.0) },
+	correct{ "01.234", NewTimecode(0, 0, 1.234) },
+	correct{ "01:02", NewTimecode(0, 1, 2.0) },
+	correct{ "01:02:03", NewTimecode(1, 2, 3.0) },
+	correct{ "-01:02:03.456", NewTimecode(-1, -2, -3.456) },
+	correct{ "-1", NewTimecode(0, 0, -1.0) },
+	correct{ "-01.234", NewTimecode(0, 0, -1.234) },
+	correct{ "-01:02", NewTimecode(0, -1, -2.0) },
+	correct{ "-01:02:03", NewTimecode(-1, -2, -3.0) },
+	correct{ "08:09:010", NewTimecode(8, 9, 10.0) },
+}
+
+var typesParseHHMMSSIncorrect = []incorrect {
+	incorrect{ "01:err:03.456", "01:err:03.456 parse error: strconv.Atoi: parsing \"err\": invalid syntax" },
+	incorrect{ "--01:02:03.456", "--01:02:03.456 parse error: strconv.Atoi: parsing \"--01\": invalid syntax" },
+	incorrect{ "01:-02:03.456", "01:-02:03.456 parse error: only first subvalue can have a sign" },
+	incorrect{ "01:02:03:4.567", "01:02:03:4.567 parse error: too many colons" },
+}
+
+var typesParseHHMMSSFrCorrect = []correct {
+	correct{ "01:02:03:04", NewTimecode(1, 2, 3.160) },
+	correct{ "1", NewTimecode(0, 0, 1.0) },
+	correct{ "01:02", NewTimecode(0, 1, 2.0) },
+	correct{ "01:02:03", NewTimecode(1, 2, 3.0) },
+	correct{ "-01:02:03:10", NewTimecode(-1, -2, -3.400) },
+	correct{ "-1", NewTimecode(0, 0, -1.0) },
+	correct{ "-01:02", NewTimecode(0, -1, -2.0) },
+	correct{ "-01:02:03", NewTimecode(-1, -2, -3.0) },
+	correct{ "08:09:010", NewTimecode(8, 9, 10.0) },
+}
+
+var typesParseHHMMSSFrIncorrect = []incorrect {
+	incorrect{ "01:err:03:04", "01:err:03:04 parse error: strconv.Atoi: parsing \"err\": invalid syntax" },
+	incorrect{ "--01:02:03:04", "--01:02:03:04 parse error: strconv.Atoi: parsing \"--01\": invalid syntax" },
+	incorrect{ "01:-02:03:04", "01:-02:03:04 parse error: only first subvalue can have a sign" },
+	incorrect{ "01:02:03:04:05", "01:02:03:04:05 parse error: too many colons" },
+}
+
+var typesParseTimecodeCorrect = []correct {
+	correct{ "01:02:03.456", NewTimecode(1, 2, 3.456) },
+	correct{ "01:02:03:04", NewTimecode(1, 2, 3.160) },
+}
+
+func testCorrect(t *testing.T, title string, fn func(string)(Timecode, error), data []correct) {
+	l := len(data)
+	for i, v := range data {
+		out, err := fn(v.in)
+		if err != nil {
+			t.Errorf("%v[#%v/%v] error: %v\n", title, i, l, err)
+			continue
+		}
+		if v.out != out {
+			t.Errorf("%v[#%v/%v] expected %v, got %v\n", title, i, l, v.out, out)
+		}
+	}
+}
+
+func testIncorrect(t *testing.T, title string, fn func(string)(Timecode, error), data []incorrect) {
+	l := len(data)
+	for i, v := range data {
+		_, err := fn(v.in)
+		if err == nil {
+			t.Errorf("%v[#%v/%v] do not have an error\n", title, i, l)
+			continue
+		}
+		if v.out != err.Error() {
+			t.Errorf("%v[#%v/%v] expected error %q, got %q\n", title, i, l, v.out, err)
+		}
+	}
+}
+
+func TestMsCorrect(t *testing.T) {
+	testCorrect(t, "ParseHHMMSS", ParseHHMMSS, typesParseHHMMSSCorrect )
+}
+
+func TestMsIncorrect(t *testing.T) {
+	testIncorrect(t, "ParseHHMMSS", ParseHHMMSS, typesParseHHMMSSIncorrect)
+}
+
+func TestFrCorrect(t *testing.T) {
+	testCorrect(t, "ParseHHMMSSFr", ParseHHMMSSFr, typesParseHHMMSSFrCorrect)
+}
+
+func TestFrIncorrect(t *testing.T) {
+	testIncorrect(t, "ParseHHMMSSFr", ParseHHMMSSFr, typesParseHHMMSSFrIncorrect)
+}
+
+func TestTypeCorrect(t *testing.T) {
+	testCorrect(t, "Parse", ParseTimecode, typesParseTimecodeCorrect)
+}
