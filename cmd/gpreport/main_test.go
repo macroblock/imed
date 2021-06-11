df287ff0fc110ebf712448770324ba6ba35314f0
mike <undefined@subject.com>
2021-06-11 16:22:50 +1000

    gpreport: fix test for windows

--- cmd/gpreport/main_test.go
+++ cmd/gpreport/main_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+    "path/filepath"
+    "strings"
     "testing"
 )
+
+const pathSep = string(filepath.Separator)
 
 var data = []string {
 	"xxx/yyy/PO/posters/project_a/google_apple_feed/jpg/g_iconic_poster_600x800.jpg",
@@ -15,6 +19,9 @@ var data = []string {
 }
 
 func TestJob(t *testing.T) {
+	for i := range data {
+		data[i] = strings.Replace(data[i], "/", pathSep, -1)
+	}
 	_, err := doJob(data)
 	if err != nil {
 		t.Error(err)
