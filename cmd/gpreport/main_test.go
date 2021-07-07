39509468871a1a74b6668ae3cae046d0de908784
mike <undefined@subject.com>
2021-07-07 17:51:59 +1000

    improve tests

--- cmd/gpreport/main_test.go
+++ cmd/gpreport/main_test.go
@@ -8,22 +8,52 @@ import (
 
 const pathSep = string(filepath.Separator)
 
-var data = []string{
+var dataIn = []string{
 	"xxx/yyy/PO/posters/project_a/google_apple_feed/jpg/g_iconic_poster_600x800.jpg",
 	"yyy/PO/posters/project_b/google_apple_feed/jpg/g_iconic_poster_600x800.jpg",
 	"/PO/posters/project_c/google_apple_feed/jpg/g_iconic_poster_600x800.jpg",
 	"PO/posters/project_d/google_apple_feed/jpg/g_iconic_poster_600x800.jpg",
 	"posters/project_a/google_apple_feed/jpg/g_iconic_poster_600x800.jpg",
+	"posters/for_service/для сервиса/600x600.jpg",
+	"posters/season/3 сезон/600x600.jpg",
+	"posters/season2/42 сезон/600x600.jpg",
+	"posters/season3/42 сезон/для сервиса/600x600.jpg",
+	"posters/season2/для сервиса/42 сезон/600x600.jpg",
 	// "/posters/project_a/google_apple_feed/jpg/g_iconic_poster_600x800.jpg",
 	// "project_a/google_apple_feed/jpg/g_iconic_poster_600x800.jpg",
 }
 
+var dataOut = []string{
+	"project_a",
+	"project_b",
+	"project_c",
+	"project_d",
+	"project_a",
+	"for_service",
+	"season",
+	"season2",
+	"season3",
+	"для сервиса",
+}
+
 func TestJob(t *testing.T) {
-	for i := range data {
-		data[i] = strings.Replace(data[i], "/", pathSep, -1)
+	for i := range dataIn {
+		dataIn[i] = strings.Replace(dataIn[i], "/", pathSep, -1)
 	}
-	_, err := doJob(data)
+	res, err := doJob(dataIn)
 	if err != nil {
 		t.Error(err)
+		return
+	}
+
+	if len(res) != len(dataOut) {
+		t.Errorf("lengths aren't equal (%v != %v)", len(res), len(dataOut))
+		return
+	}
+	for i, line := range res {
+		name := strings.Split(line, "\t")[0]
+		if name != dataOut[i] {
+			t.Errorf("data line %v: %q != %q)", i, name, dataOut[i])
+		}
 	}
 }
