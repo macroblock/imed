b4f39fbaab8a8bc97f18bf68276aa54f06630257
mike <undefined@subject.com>
2023-05-05 14:09:08 +1000

    subrip: fix parser error at EOF

--- pkg/subrip/parser.go
+++ pkg/subrip/parser.go
@@ -34,16 +34,17 @@ func Parse(rd io.Reader) ([]Record, error) {
 	scanner := bufio.NewScanner(br)
 	state := stReady
 	ln := 0
+	line := ""
 	r := Record{}
 	ret := []Record{}
 	for scanner.Scan() {
-		line := scanner.Text()
+		line = scanner.Text()
 		ln++
 		switch state {
 		default:
 			panic("unreachable")
 		case stReady:
-			if len(line) == 0 {
+			if len(strings.TrimSpace(line)) == 0 {
 				continue
 			}
 			// chunk number
@@ -69,13 +70,13 @@ func Parse(rd io.Reader) ([]Record, error) {
 			}
 			state = stTextStart
 		case stTextStart:
-			if len(line) == 0 {
+			if len(strings.TrimSpace(line)) == 0 {
 				return nil, parseError(ln, line, "text")
 			}
 			r.Text = line
 			state = stText
 		case stText:
-			if len(line) != 0 {
+			if len(strings.TrimSpace(line)) != 0 {
 				r.Text += "\n" + line
 				continue
 			}
@@ -88,8 +89,8 @@ func Parse(rd io.Reader) ([]Record, error) {
 		return nil, err
 	}
 
-	if state != stReady {
-		return nil, fmt.Errorf("unexpected <EOF>")
+	if state != stReady && state != stText{
+		return nil, fmt.Errorf("line %v: unexpected <EOF>", ln)
 	}
 	return ret, nil
 }
