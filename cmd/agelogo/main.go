6337a7516602b301946ce7fa1dcd5d9f78376048
mike <undefined@subject.com>
2024-03-01 17:24:59 +1000

    agelogo: fix aud/sub tags issue

--- cmd/agelogo/main.go
+++ cmd/agelogo/main.go
@@ -78,9 +78,12 @@ func doProcess(filePath string, isDeepCheck bool) string {
 	subTag := ""
 	subTag, err = tn.GetTag("stag")
 
-	audsubPostfix := audTag
-	if subTag != "" {
-		audsubPostfix += "_" + subTag
+	aCTag := cleanAudSubTag(subTag)
+	sCTag := cleanAudSubTag(audTag)
+
+	audsubPostfix := aCTag
+	if sCTag != "" {
+		audsubPostfix += "_" + sCTag
 	}
 
 	sdhd, err := tn.GetTag("sdhd")
@@ -156,7 +159,7 @@ func doProcess(filePath string, isDeepCheck bool) string {
 	if hasSmokingTag {
 		path := filepath.Join(ageLogoPath, "smk",
 			"msmoking_"+logoPostfix+"_"+audsubPostfix+".mp4")
-		// workaround: replace windows backslashes to use it in ffmpeg filter
+		// workaround: replace windows backslashes to use it with ffmpeg filter
 		path = strings.Replace(path, "\\", "/", -1)
 		ret += fmt.Sprintf("echo file %v %v #fflist.txt\n", path, redir)
 		redir = ">>"
@@ -165,7 +168,7 @@ func doProcess(filePath string, isDeepCheck bool) string {
 	if hasAlcoholTag {
 		path := filepath.Join(ageLogoPath, "alcohol",
 			"alcohol_"+logoPostfix+"_"+audsubPostfix+".mp4")
-		// workaround: replace windows backslashes to use it in ffmpeg filter
+		// workaround: replace windows backslashes to use it with ffmpeg filter
 		path = strings.Replace(path, "\\", "/", -1)
 		ret += fmt.Sprintf("echo file %v %v #fflist.txt\n", path, redir)
 		redir = ">>"
@@ -182,141 +185,6 @@ func doProcess(filePath string, isDeepCheck bool) string {
 	return ret
 }
 
-/*
-func doProcess(filePath string, isDeepCheck bool) string {
-	defer retif.Catch()
-	log.Info(" ")
-	log.Info("processing: " + filePath)
-	tn, err := tagname.NewFromFilename(filePath, isDeepCheck)
-	retif.Error(err, "cannot parse filename")
-
-	// schema := "" //tn.Schema()
-
-	_, err = tn.GetTag("alreadyagedtag")
-	doAge := true
-	if err != nil {
-		doAge = false
-	}
-
-	age := ""
-	if !doAge {
-		age, err = tn.GetTag("agetag")
-		retif.Error(err, "cannot get 'agetag' tag")
-	}
-
-	sdhd, err := tn.GetTag("sdhd")
-	retif.Error(err, "cannot get 'sdhd' tag")
-
-	qtag, err := tn.GetTag("qtag")
-
-	hasSmokingTag := false
-	if _, err := tn.GetTag("smktag"); err == nil {
-		hasSmokingTag = true
-	}
-	hasSideBySideTag := false
-	if _, err := tn.GetTag("sbstag"); err == nil {
-		hasSideBySideTag = true
-	}
-
-	retif.Error(!hasSmokingTag, "nothing to do")
-
-	// tn.RemoveTags("agetag")
-	// tn.RemoveTags("alreadyagedtag")
-	tn.RemoveTags("smktag")
-
-	newPath, err := tn.ConvertTo("")
-	retif.Error(err, "cannot convert to '"+tn.Schema()+"' schema")
-
-	file, err := ffinfo.Probe(filePath)
-	retif.Error(err, "ffinfo.Probe() (ffprobe)")
-
-	sar := ""
-	logoPostfix := ""
-	sbsPostfix := ""
-	err = fmt.Errorf("%v", "cannot find video stream")
-	for i, s := range file.Streams {
-		if s.CodecType != "video" {
-			continue
-		}
-		sar = s.SampleAspectRatio
-		log.Notice(fmt.Sprintf("stream: %v, sar [%v]", i, sar))
-		switch sar {
-		default:
-			retif.Error(fmt.Errorf("inconvenient SAR [%v]", sar))
-		case "64:45":
-			logoPostfix = "169"
-		case "16:15":
-			logoPostfix = "43"
-		case "0:1", "1:1", "":
-			sar = "1:1"
-			switch sdhd {
-			default:
-				retif.Error(fmt.Errorf("inconvenient set of SAR [%v] and sdhd tag %q", sar, sdhd))
-			case "3d":
-				logoPostfix = "3D"
-				if hasSideBySideTag {
-					sbsPostfix = "_SBS"
-				}
-			case "hd":
-				logoPostfix = "HD"
-			}
-		}
-		err = nil
-		break
-	}
-	retif.Error(err)
-	strSar := strings.Replace(sar, ":", "/", -1) // x:y -> x/y
-
-	x := "-"
-	if qtag != "" {
-		x = string(qtag[2])
-	}
-	if !(x == "-" ||
-		x == "s" && in(logoPostfix, "43") ||
-		x == "w" && in(logoPostfix, "169", "HD", "3D")) {
-		retif.Error(fmt.Errorf("wrong qtag %q for %v video", qtag, logoPostfix))
-	}
-
-	strVCodec := "#error#"
-	strACodec := "#error#"
-	switch logoPostfix {
-	default:
-		retif.Error(fmt.Errorf("unreachable"))
-	case "HD", "3D":
-		strVCodec = "-vcodec libx264 -preset medium -crf 16 -pix_fmt yuv420p -g 0"
-		strACodec = "-acodec ac3 -ab 320k"
-	case "43", "169":
-		strVCodec = "-vcodec mpeg2video -b:v 11000k -maxrate 15000k -minrate 0 -bufsize 1835008 -rc_init_occupancy 600000 -g 12 -bf 2 -q:v 1 -pix_fmt yuv420p"
-		strACodec = "-acodec mp2 -ab 320k"
-	}
-
-	strSmoking := ""
-	if hasSmokingTag {
-		smkImg := filepath.Join(ageLogoPath, "msmoking_"+logoPostfix+sbsPostfix+".mov")
-		// workaround: replace windows backslashes to use it in ffmpeg filter
-		smkImg = strings.Replace(smkImg, "\\", "/", -1)
-		strSmoking = "; movie=" + smkImg + ",setsar=" + strSar + "[smoking]; " +
-			" anullsrc=r=48000:cl=mono,atrim=end=5,pan=2c|c0=c0|c1=c0[silence]; [smoking][silence][v][a]concat=n=2:v=1:a=1[v][a]; [v]setsar=" + strSar + "[v]"
-	}
-
-	logo := filepath.Join(ageLogoPath, age+"_"+logoPostfix+sbsPostfix+".mov")
-	// workaround: replace windows backslashes to use it in ffmpeg filter
-	logo = strings.Replace(logo, "\\", "/", -1)
-	// ret := "fflite -i \"" + filePath + "\" -filter_complex \"movie=" + logo + ",setsar=" + strSar +
-	// "[age]; [0:0][age]overlay=0:0:eof_action=pass[v]; [0:1]aresample=48000[a]" + strSmoking + "\" " +
-	// " -map [v] " + strVCodec + " -map [a] " + strACodec + " " + newPath
-
-	// strAgeLogo := "; movie=" + logo + ",setsar=" + strSar +
-	// "[age]; [v][age]overlay=0:0:eof_action=pass[v]; [a]aresample=48000[a]"
-
-	strAgeLogo := ""
-	ret := "fflite -i \"" + filePath + "\" -filter_complex \"[0:0]null[v]; [0:1]anull[a]" +
-		strAgeLogo + strSmoking + "\" " +
-		" -map [v] " + strVCodec + " -map [a] " + strACodec + " " + newPath
-
-	return ret
-}
-*/
 
 func mainFunc() error {
 	if len(flagFiles) == 0 {
@@ -385,4 +253,19 @@ func main() {
 
 	log.Error(err)
 	log.Info(cmdLine.GetHint())
+}
+
+
+func cleanAudSubTag(tag string) string {
+	if tag == "" {
+		return ""
+	}
+	ret := tag[0:1]
+
+	for _, r := range tag[1:len(tag)] {
+		if r >= '0' && r <= '9' {
+			ret += string(r)
+		}
+	}
+	return ret
 }
